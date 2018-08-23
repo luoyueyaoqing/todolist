5f9311b0cd12d17d2b7f6804ed29927296de9065
Li Huilong <shmilyfae@163.com>
2018-08-23 18:05:19 +0800

    创建todolist页面

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Task(models.Model):
+    task = models.CharField(max_length=128)
+    complete = models.BooleanField()
+    create_time = models.DateTimeField(default=timezone.now)
+    complete_time = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return '{}-{}'.format(self.complete, self.task[:20])
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'index.html', {'tasks': tasks})
